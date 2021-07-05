1aed2fba47408bbc0f0e4c589dcf343dd7b5d64e
Prasetya Aji Baskara <prasetya.baskara@btpn.com>
2021-07-05 12:52:39 +0700

    handling error

--- public/js/app.js
+++ public/js/app.js
@@ -4,26 +4,35 @@
   const weatherForm = document.querySelector('form');
   const formResult = document.getElementById('form-result');
   const formIcon = document.querySelector('#form-icon');
+  const addressInput = document.getElementById('address');
 
   weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const address = document.getElementById('address').value;
+    
+    const address = addressInput.value;
     formResult.innerHTML = 'Please wait..';
     formIcon.src = '';
 
-    fetch(`/weather?address=${address}`).then(response => {
-      response.json().then(data => {
-        if (data.error) {
-          formResult.innerHTML = data.error;
+    if (address) {
+      fetch(`asd/weather?address=${address}`).then(response => {
+        response.json().then(data => {
+          if (data.error) {
+            formResult.innerHTML = data.error;
+            formIcon.src = '';
+          } else {
+            const { temperature, weather_descriptions, weather_icons } = data;
+  
+            formResult.innerHTML = `it's ${temperature} celcius, and ${weather_descriptions[0]}`;
+            formIcon.src = weather_icons[0];
+          }
+        }).catch(error => {
+          formResult.innerHTML = 'Something went wrong, Try again';
           formIcon.src = '';
-        } else {
-          const { temperature, weather_descriptions, weather_icons } = data;
-
-          formResult.innerHTML = `it's ${temperature} celcius, and ${weather_descriptions[0]}`;
-          formIcon.src = weather_icons[0];
-        }
+        });
       });
-    });
+    } else {
+      formResult.innerHTML = 'Please input Location';
+      formIcon.src = '';
+    }
   });
-
 })();
